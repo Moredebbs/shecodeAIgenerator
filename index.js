351978265c74dbfeaf7118b1717413504487ed42
Moredebbs <90006497+Moredebbs@users.noreply.github.com>
2024-05-05 22:10:23 +0100

    added basic form ending and typewriter plugin

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-//alert("hello");
+function generatePoem(event) {
+  event.preventDefault();
+  //alert("generating poem");
+
+  new Typewriter("#poem", {
+    strings: "Poem goes here",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
