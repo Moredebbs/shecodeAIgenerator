7e45400c5c052de6ebf240971d99c284030a0920
Moredebbs <90006497+Moredebbs@users.noreply.github.com>
2024-05-07 13:20:05 +0100

    added AI API intefration

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-function generatePoem(event) {
-  event.preventDefault();
-  //alert("generating poem");
-
+function displayPoem(response) {
+  //console.log("poem generated");
   new Typewriter("#poem", {
-    strings: "Poem goes here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+  //alert("generating poem");
+
+  let instructionInput = document.querySelector("#instruction-input");
+  let apiKey = "80428f51a21b4t3dfe9d3b547of6cb3f";
+  let prompt = `user instructions: Generate a short poem about${instructionInput.value}`;
+  let context =
+    " you are a poem expert and love to write short powerful poem . please generate four lines of poem in basic HTML and seperate each line with a <br />. Make sure to follow the user instructions";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  //console.log("Generating poem");
+  // console.log(`prompt:${prompt}`);
+  //console.log(`Context:${context}`);
+  axios.get(apiURL).then(displayPoem);
 }
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
