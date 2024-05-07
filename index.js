be1c02ea6425d41d69e27801a7f9e41b47d7d10c
Moredebbs <90006497+Moredebbs@users.noreply.github.com>
2024-05-07 14:12:35 +0100

    improved the user experience

--- index.js
+++ index.js
@@ -16,8 +16,11 @@ function generatePoem(event) {
   let apiKey = "80428f51a21b4t3dfe9d3b547of6cb3f";
   let prompt = `user instructions: Generate a short poem about${instructionInput.value}`;
   let context =
-    " you are a poem expert and love to write short powerful poem . please generate four lines of poem in basic HTML and seperate each line with a <br />. Make sure to follow the user instructions";
+    " you are a poem expert and love to write short powerful poem . please generate four lines of poem in basic HTML and seperate each line with a <br />. Make sure to follow the user instructions and please display poem only. please at the end of the poem sign it by <strong>MoyOwoeye</strong>";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a poem about ${instructionInput.value} for you......</div>`;
   //console.log("Generating poem");
   // console.log(`prompt:${prompt}`);
   //console.log(`Context:${context}`);
